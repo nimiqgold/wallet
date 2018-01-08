d4403563fe2f69d158c46ebdf333363f9b4df3a7
danimoh <github.danimoh@neverbox.com>
2018-01-08 20:31:54 +0100

    position of file upload button

--- elements/view-send/view-send.js
+++ elements/view-send/view-send.js
@@ -3,10 +3,13 @@ class ViewSend extends XElement {
 
     onCreate() {
         this.$qrScanner.setGrayscaleWeights(145, 91, 20);
+        this.$qrScanner.hasFileInputButton = false;
         this.addEventListener('x-decoded', e => this._validateAddress(e.detail));
-        this.$input = this.$('input');
+        this.$input = this.$('input[type="text"]');
         this.$input.addEventListener('input', e => this._validateAddress(e.target.value));
         this.$('x-header a').addEventListener('click', e => this.$input.focus());
+        this.$fileSelector = this.$('input[type="file"]');
+        this.$fileSelector.addEventListener('change', () => this._onFileSelected());
     }
 
     onShow() {
@@ -22,7 +25,17 @@ class ViewSend extends XElement {
         if (!NanoApi.validateAddress(address)) return;
         this.fire('x-recipient', address);
     }
+
+    _onFileSelected() {
+        const file = this.$fileSelector.files[0];
+        this.$fileSelector.value = ''; // reset the file selector
+        if (!file) {
+            return;
+        }
+        this.$qrScanner.scanImage(file);
+    }
 }
 
 // Todo: don't allow value > balance
 // Todo: add debouncer to input handler
+// Todo: maybe show an error message if in uploaded file no address was found
