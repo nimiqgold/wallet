fab4d9fb90eef77d4455232d38781daf0138d667
danimoh <github.danimoh@neverbox.com>
2018-01-13 17:39:14 +0100

    view-send: error message on scan error

--- elements/view-send/view-send.js
+++ elements/view-send/view-send.js
@@ -5,11 +5,14 @@ class ViewSend extends XElement {
         this.$qrScanner.setGrayscaleWeights(145, 91, 20);
         this.$qrScanner.hasFileInputButton = false;
         this.addEventListener('x-decoded', e => this._validateAddress(e.detail));
+        this.addEventListener('x-image-decoded', e => this._onFileScanned(event));
+        this.addEventListener('x-image-error', e => this._onFileScanned(event));
         this.$input = this.$('input[type="text"]');
         this.$input.addEventListener('input', e => this._validateAddress(e.target.value));
-        this.$('x-header a').addEventListener('click', e => this.$input.focus());
+        this.$('x-header [icon-paste]').addEventListener('click', e => this.$input.focus());
         this.$fileSelector = this.$('input[type="file"]');
         this.$fileSelector.addEventListener('change', () => this._onFileSelected());
+        this.$fileSelectorIcon = this.$('[icon-upload]');
     }
 
     onShow() {
@@ -22,17 +25,27 @@ class ViewSend extends XElement {
     }
 
     _validateAddress(address) {
-        if (!NanoApi.validateAddress(address)) return;
+        if (!NanoApi.validateAddress(address)) return false;
         this.fire('x-recipient', address);
+        return true;
     }
 
     _onFileSelected() {
         const file = this.$fileSelector.files[0];
-        this.$fileSelector.value = ''; // reset the file selector
+        this.$fileSelector.value = null; // reset the file selector
         if (!file) {
             return;
         }
         this.$qrScanner.scanImage(file);
+    }
+
+    _onFileScanned(event) {
+        if (event.type === 'x-image-decoded' && this._validateAddress(event.detail)) {
+            return;
+        }
+        // no success
+        this.$fileSelectorIcon.classList.add('warning');
+        setTimeout(() => this.$fileSelectorIcon.classList.remove('warning'), 4000);
     }
 
     html() {
@@ -48,5 +61,5 @@ class ViewSend extends XElement {
 
 // Todo: don't allow value > balance
 // Todo: add debouncer to input handler
-// Todo: maybe show an error message if in uploaded file no address was found
 // Todo: Debug for iOS
+// Todo: Bug on repeated click onto the 'send' tab (camera activated multiple times)
