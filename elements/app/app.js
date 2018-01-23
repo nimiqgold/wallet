8f297c6496dc3ea345bc3ec4a0a5c3379e488dbd
robinlinus <robinlinus@users.noreply.github.com>
2018-01-23 04:05:54 -0600

    Cleanup app html

--- elements/app/app.js
+++ elements/app/app.js
@@ -19,47 +19,47 @@ import XInactivitySensor from '/elements/x-inactivity-sensor/x-inactivity-sensor
 export default class Wallet extends XApp {
     html() {
         return `
-        <x-blur-container>
-            <header>
-                <nimiq-logo></nimiq-logo>
-            </header>
-            <x-app-container>
-                <main>
-                    <view-home></view-home>
-                    <view-receive></view-receive>
-                    <view-send></view-send>
-                    <view-permission></view-permission>
-                    <view-transaction></view-transaction>
-                    <view-confirm></view-confirm>
-                    <view-fees></view-fees>
-                    <view-success></view-success>
-                    <view-history></view-history>
-                    <!-- Onboarding -->
-                    <view-welcome></view-welcome>
-                    <view-identicons></view-identicons>
-                    <!-- Backup -->
-                    <view-backup-file></view-backup-file>
-                    <view-backup-file-import></view-backup-file-import>
-                    <!-- Notifications -->
-                    <view-loading></view-loading>
-                    <view-error></view-error>
-                </main>
-                <nav>
-                    <x-nav-box>
-                        <a href="#receive" tabindex="1">Receive</a>
-                        <a href="#home">Home</a>
-                        <a href="#send">Send</a>
-                    </x-nav-box>
-                </nav>
-            </x-app-container>
-        </x-blur-container>
-        <view-received></view-received>
-        <view-locked></view-locked>
-        <view-pin-create></view-pin-create>
-        <view-pin-change></view-pin-change>
-        <x-inactivity-sensor></x-inactivity-sensor>
-        <x-nimiq-api connect="true"></x-nimiq-api>
-        <noscript><link href="/nimiq-elements/noscript/noscript.css" rel="stylesheet"></noscript>
+            <x-blur-container>
+                <header>
+                    <nimiq-logo></nimiq-logo>
+                </header>
+                <x-app-container>
+                    <main>
+                        <view-home></view-home>
+                        <view-receive></view-receive>
+                        <view-send></view-send>
+                        <view-permission></view-permission>
+                        <view-transaction></view-transaction>
+                        <view-confirm></view-confirm>
+                        <view-fees></view-fees>
+                        <view-success></view-success>
+                        <view-history></view-history>
+                        <!-- Onboarding -->
+                        <view-welcome></view-welcome>
+                        <view-identicons></view-identicons>
+                        <!-- Backup -->
+                        <view-backup-file></view-backup-file>
+                        <view-backup-file-import></view-backup-file-import>
+                        <!-- Notifications -->
+                        <view-loading></view-loading>
+                        <view-error></view-error>
+                    </main>
+                    <nav>
+                        <x-nav-box>
+                            <a href="#receive" tabindex="1">Receive</a>
+                            <a href="#home">Home</a>
+                            <a href="#send">Send</a>
+                        </x-nav-box>
+                    </nav>
+                </x-app-container>
+            </x-blur-container>
+            <view-received></view-received>
+            <view-locked></view-locked>
+            <view-pin-create></view-pin-create>
+            <view-pin-change></view-pin-change>
+            <x-inactivity-sensor></x-inactivity-sensor>
+            <x-nimiq-api connect="true"></x-nimiq-api>
+            <noscript><link href="/elements/noscript/noscript.css" rel="stylesheet"></noscript>
         `
     }
 
