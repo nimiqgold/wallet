f602c742eb56818d75fbd07145e4d6b6b26159fa
robinlinus <robinlinus@users.noreply.github.com>
2018-02-02 17:25:56 -0600

    Cleanup screen welcome

--- elements/screen-welcome/screen-welcome.js
+++ elements/screen-welcome/screen-welcome.js
@@ -3,11 +3,9 @@ import XScreen from '/elements/x-screen/x-screen.js';
 export default class ScreenWelcome extends XScreen {
     html() {
         return `
-			<x-container>
-			    <nimiq-logo large>Nimiq Wallet</nimiq-logo>
-			    <h1>Welcome to Nimiq</h1>
-			    <h2>Nimiq is a peer to peer digital currency. This wallet app will connect you to the network and help you store and transfer Nimiqs.</h2>
-			</x-container>
+		    <nimiq-logo large>Nimiq Wallet</nimiq-logo>
+		    <h1>Welcome to Nimiq</h1>
+		    <h2>Nimiq is a peer to peer digital currency. This wallet app will connect you to the network and help you store and transfer Nimiqs.</h2>
 			<a button href="#identicons">Create Account</a>
 			<a secondary href="#backup-file-import">Import existing Account</a>
 		`
